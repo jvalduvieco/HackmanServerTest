a5a8e4d265e9cf5cf23be2e83b2dcb45f2b6d8d3
Joan Valduvieco <jvalduvieco@gmail.com>
2013-04-19 11:00:35 +0200

    More work

---
+++ include/client_state.hrl
@@ -0,0 +1,12 @@
+-record(client_state, {
+	session_id = none,
+	websocket_client_pid = none,
+	game_id = none,
+	player_id = none,
+	match_id = none,
+	x = 0,
+	y = 0,
+	vx = 0,
+	vy = 0,
+	move_timer = none
+}).
--- src/hackman_client.erl
+++ src/hackman_client.erl
@@ -8,7 +8,8 @@
 -export([start_link/0, send_event/3]).
 
 %% gen_fsm
--export([init/1, waiting_connect/2, waiting_login/2, handle_event/3, state_name/3, waiting_create_match/2,waiting_new_player/2, waiting_start_match/2, playing/2,
+-export([init/1, waiting_connect/2, waiting_login/2, handle_event/3, state_name/3, waiting_create_match/2, waiting_new_player/2,
+	waiting_get_objects/2, waiting_start_match/2, playing/2,
 	handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
 
 -define(HS_CLIENT_MOVE_TIME, 16).
@@ -50,7 +51,10 @@ waiting_login({<<"loginResponse">>, Data}, State) ->
 waiting_create_match ({<<"newMatchResponse">>, _Data}, State) ->
 	hst_websocket_client:send_message(State#client_state.websocket_client_pid, hst_messages_composer:new_player(State)),
 	{next_state, waiting_new_player, State}.
-waiting_new_player ({<<"newPlayerResponse">>, _Data}, State)->
+waiting_new_player ({<<"newPlayerResponse">>, _Data}, State) ->
+	hst_websocket_client:send_message(State#client_state.websocket_client_pid, hst_messages_composer:get_objects(State)),
+	{next_state, waiting_get_objects, State}.
+waiting_get_objects({<<"getObjectsResponse">>, _Data}, State) ->
 	{next_state, waiting_start_match, State}.
 waiting_start_match({<<"startMatch">>, _Data}, State) ->
 	{X, Y} = {State#client_state.x, State#client_state.y},
@@ -68,7 +72,14 @@ playing({timeout, _Ref, move_timeout}, State) ->
 	hst_websocket_client:send_message(NewState#client_state.websocket_client_pid, Message),
 	MoveTimer = create_move_timer(),
 	{next_state, playing, NewState#client_state{move_timer = MoveTimer}};
-playing(_Message, State) ->
+playing({<<"positionUpdateAnnounce">>, _Data}, State) ->
+	{next_state, playing, State};
+playing({<<"endMatch">>, _Data}, State) ->
+	gen_fsm:cancel_timer(State#client_state.move_timer),
+	hst_websocket_client:send_message(State#client_state.websocket_client_pid, hst_messages_composer:get_objects(State)),
+	{next_state, waiting_get_objects, State#client_state{move_timer = none}};
+playing(Event, State) ->
+	lager:debug("Unhandled event ~p",[Event]),
 	{next_state, playing, State}.
 
 state_name(_Event, _From, State) ->
